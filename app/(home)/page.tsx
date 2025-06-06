f24b9ce0e97c914c567f6c2bcd69885b0e18fdab
TheUltDev <2079305+TheUltDev@users.noreply.github.com>
2025-06-06 17:09:13 -0500

    troubleshoot ux

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,9 +44,9 @@ export default function HomePage() {
             <HomeBox
               title="Troubleshooting"
               description="Check if you're experiencing common issues."
-              iconPath="M13 10V3L4 14h7v7l9-11h-7z"
-              iconBgColor="bg-indigo-100"
-              iconColor="text-indigo-600"
+              iconPath="m8 2 1.88 1.88M14.12 3.88 16 2M9 7.13v-1a3.003 3.003 0 1 1 6 0v1M12 20c-3.3 0-6-2.7-6-6v-3a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v3c0 3.3-2.7 6-6 6M12 20v-9M6.53 9C4.6 8.8 3 7.1 3 5M6 13H2M3 21c0-2.1 1.7-3.9 3.8-4M20.97 5c0 2.1-1.6 3.8-3.5 4M22 13h-4M17.2 17c2.1.1 3.8 1.9 3.8 4"
+              iconBgColor="bg-red-100"
+              iconColor="text-red-600"
               borderClasses="border-b sm:border-b-0 border-fd-border"
             />
           </div>
